09c52436a0112bc78daf9b29cbe018463eea395b
Nikita P <nikita.panasevich22@gmail.com>
2023-05-03 17:39:50 +0300

    arialabels

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,22 +7,34 @@ import ic from '../../images/icons.svg';
 const index = () => (
 	<Footer>
 		<LinkCont>
-			<Link href='https://uk-ua.facebook.com' target='_blank'>
+			<Link
+				href='https://uk-ua.facebook.com'
+				target='_blank'
+				aria-label='Link to our facebook page'>
 				<Icon>
 					<use href={ic + '#facebook' } />
 				</Icon>
 			</Link>
-			<Link href='https://twitter.com/home' target='_blank'>
+			<Link
+				href='https://twitter.com/home'
+				target='_blank'
+				aria-label='Link to our twitter page'>
 				<Icon>
 					<use href={ic + '#twitter' } />
 				</Icon>
 			</Link>
-			<Link href='https://www.youtube.com' target='_blank'>
+			<Link
+				href='https://www.youtube.com'
+				target='_blank'
+				aria-label='Link to our youtube page'>
 				<IconYT>
 					<use href={ic + '#youtube' } />
 				</IconYT>
 			</Link>
-			<Link href='https://www.linkedin.com' target='_blank'>
+			<Link
+				href='https://www.linkedin.com'
+				target='_blank'
+				aria-label='Link to our linkedin page'>
 				<IconIn>
 					<use href={ic + '#linkedin' } />
 				</IconIn>
